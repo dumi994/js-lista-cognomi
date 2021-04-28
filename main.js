b45e5540f73ba3ebb7be5823dc44d531d7bd9878
dumi994 <dumi.9944@gmail.com>
2021-04-28 16:51:34 +0200

    inserita la posizone umana

--- main.js
+++ main.js
@@ -15,4 +15,10 @@ for (var i = 0; i < listaCognomi.length; i++){
     var content = document.getElementById('listaCognomi').innerHTML;
     document.getElementById('listaCognomi').innerHTML = content + '<li>' + nuovaLista + '</li>';
 }
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+var posizione = listaCognomi.indexOf(cognomeUtente); // posizione nell'array
+var posizione = listaCognomi.indexOf(cognomeUtente) + 1; //posizione umana
+console.log(posizione);
+
+document.getElementById('posizione').innerHTML = 'Il tuo cognome si trova in posizione ' + posizione;
