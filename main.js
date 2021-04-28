ad2086100f90390f62bd00ee77dbc61b64ceece7
dumi994 <dumi.9944@gmail.com>
2021-04-28 15:46:05 +0200

    aggiunto innerHTML che stampa il cognome

--- main.js
+++ main.js
@@ -5,11 +5,10 @@ var listaCognomi = ['bianchi', 'verdi', 'rossi', 'gialli', 'neri']
 var cognomeUtente = prompt('Inserisci il tuo cognome');
 console.log(cognomeUtente);
 
-
-listaCognomi.push(cognomeUtente)
-console.log(listaCognomi);
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-
+listaCognomi.push(cognomeUtente)
+console.log(listaCognomi.sort());
 // 3. stampa la lista ordinata alfabeticamente
+document.getElementById('titolo').innerHTML = 'Benvenuto ' + cognomeUtente + ', sei stato aggiunto alla lista invitati';
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
